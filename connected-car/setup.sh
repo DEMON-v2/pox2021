18708c5f6f577901e33cce838d3dceb0dcfd849f
DongHyeon Jeong <stjhyeon@kakao.com>
2021-10-30 13:35:30 +0900

    [modified] connected-car/setup.sh

--- connected-car/setup.sh
+++ connected-car/setup.sh
@@ -4,6 +4,17 @@ echo "[Challenge] Setup Virtaul CAN Device"
 
 sudo apt install can-utils -y
 
+sleep 3
+
+if mkdir ./utils ; then
+	echo "[Challenge] Create Utils Directory"
+fi
+
+cp /usr/bin/cansniffer ./utils
+cp /usr/bin/candump ./utils
+cp /usr/bin/cansend ./utils
+cp /usr/bin/canplayer ./utils
+
 sudo modprobe vcan
 sudo ip link add dev vcan0 type vcan
 sudo ip link set up vcan0
