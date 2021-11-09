8975199d6308d0455f3abb285ebdbded6cf8e0d7
DongHyeon Jeong <stjhyeon@kakao.com>
2021-11-09 15:29:46 +0900

    [수정] poc2021/flagbot_v2 로직 추가

--- flagbot_v2/run.py
+++ flagbot_v2/run.py
@@ -16,6 +16,7 @@ async def on_ready():
 @app.event
 async def on_command_error(ctx, error):
     embed = Embed(title="Error!", color=0xff0000)
+    print(error)
     if isinstance(error, commands.CommandNotFound):
         embed.add_field(name="Command Not Found", value="올바른 명령어를 입력해주세요 ㅜ^ㅜ")
     await ctx.send(embed=embed)
@@ -27,6 +28,7 @@ async def help(ctx):
         e.add_field(name="!calendar", value="POX 2021 대회 시작과 종료 일정을 알려줘요!", inline=False)
         e.add_field(name="!flag1", value="플래그 주세요 #1", inline=False)
         e.add_field(name="!flag2", value="플래그 주세요 #2", inline=False)
+        e.add_field(name="!verify", value="사용자 권한을 확인합니다.", inline=False)
         e.set_footer(text="CopyRight Demon TEAM & SISS")
         await ctx.send(embed=e)
     else:
@@ -74,6 +76,23 @@ async def flag2(ctx):
         e.add_field(name="오류!", value="인증되지 않은 사용자 입니다.", inline=False)
         e.set_footer(text="CopyRight Demon TEAM & SISS")
         await ctx.author.send(embed=e)
+
+@app.command()
+async def verify(ctx):
+    if channel_check(ctx.channel.id) != False:
+        organizer = get(ctx.guild.roles, name="Organizer")
+        if organizer in ctx.author.roles:
+            e = Embed(title="POX 2021", url="https://ctf.powerofxx.org", description="안녕하세요! POX 2021 도우미 입니다.", color=0x1d87d7)
+            e.add_field(name="Check Permission", value=str(ctx.author) + " 님은 관리자입니다.", inline=False)
+            e.set_footer(text="CopyRight Demon TEAM & SISS")
+            await ctx.send(embed=e)
+        else:
+            e = Embed(title="POX 2021", url="https://ctf.powerofxx.org", description="안녕하세요! POX 2021 도우미 입니다.", color=0xff0000)
+            e.add_field(name="오류!", value=str(ctx.author) + " 님은 인증되지 않은 사용자 입니다. (your not Organizer permission)", inline=False)
+            e.set_footer(text="CopyRight Demon TEAM & SISS")
+            await ctx.send(embed=e)
+    else:
+        await ctx.send("명령어를 사용할 수 없는 채널입니다. #flag봇 채널을 사용해주세요.")
 
 if __name__ == "__main__":
     app.run(token_load())
