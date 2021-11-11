91f9a21ce4f2d1bd21fd6467533c54c696e37a0a
DongHyeon Jeong <stjhyeon@kakao.com>
2021-11-11 12:27:27 +0900

    [수정] pox2021/flagbot_v2 문제 오류 수정

--- flagbot_v2/run.py
+++ flagbot_v2/run.py
@@ -23,53 +23,41 @@ async def on_command_error(ctx, error):
 
 @app.command()
 async def help(ctx):
-    if channel_check(ctx.channel.id) != False:
-        e = Embed(title="POX 2021", url="https://ctf.powerofxx.org", description="안녕하세요! POX 2021 도우미 입니다.", color=0x1d87d7)
-        e.add_field(name="!calendar", value="POX 2021 대회 시작과 종료 일정을 알려줘요!", inline=False)
-        e.add_field(name="!flag1", value="플래그 주세요 #1", inline=False)
-        e.add_field(name="!flag2", value="플래그 주세요 #2", inline=False)
-        e.add_field(name="!verify", value="사용자 권한을 확인합니다.", inline=False)
-        e.set_footer(text="CopyRight Demon TEAM & SISS")
-        await ctx.send(embed=e)
-    else:
-        await ctx.send("명령어를 사용할 수 없는 채널입니다. #flag봇 채널을 사용해주세요.")
+    e = Embed(title="POX 2021", url="https://ctf.powerofxx.org", description="안녕하세요! POX 2021 도우미 입니다.", color=0x1d87d7)
+    e.add_field(name="!calendar", value="POX 2021 대회 시작과 종료 일정을 알려줘요!", inline=False)
+    e.add_field(name="!flag1", value="플래그 주세요 #1", inline=False)
+    e.add_field(name="!flag2", value="플래그 주세요 #2", inline=False)
+    e.add_field(name="!verify", value="사용자 권한을 확인합니다.", inline=False)
+    e.set_footer(text="CopyRight Demon TEAM & SISS")
+    await ctx.send(embed=e)
 
 @app.command()
 async def calendar(ctx):
-    if channel_check(ctx.channel.id) != False:
-        e = Embed(title="POX 2021", url="https://ctf.powerofxx.org", description="안녕하세요! POX 2021 도우미 입니다.", color=0x1d87d7)
-        e.add_field(name="일정 안내", value="POX 2021 " + "\n " + "Start : 2021/11/11 (AM) 11:11 " + "\n" + "End : 2021/11/11 (PM) 11:11", inline=False)
-        e.set_footer(text="CopyRight Demon TEAM & SISS")
-        await ctx.send(embed=e)
-    else:
-        await ctx.send("명령어를 사용할 수 없는 채널입니다. #flag봇 채널을 사용해주세요.")    
+    e = Embed(title="POX 2021", url="https://ctf.powerofxx.org", description="안녕하세요! POX 2021 도우미 입니다.", color=0x1d87d7)
+    e.add_field(name="일정 안내", value="POX 2021 " + "\n " + "Start : 2021/11/11 (AM) 11:11 " + "\n" + "End : 2021/11/11 (PM) 11:11", inline=False)
+    e.set_footer(text="CopyRight Demon TEAM & SISS")
+    await ctx.send(embed=e)  
 
 @app.command()
 async def flag1(ctx):
-    if channel_check(ctx.channel.id) != False:
-        e = Embed(title="POX 2021", url="https://ctf.powerofxx.org", description="안녕하세요! POX 2021 도우미 입니다.", color=0x1d87d7)
-        e.add_field(name="플래그 주세요 #1", value=flag_1, inline=False)
-        e.set_footer(text="CopyRight Demon TEAM & SISS")
-        await ctx.author.send(embed=e)
-    else:
-        await ctx.send("명령어를 사용할 수 없는 채널입니다. #flag봇 채널을 사용해주세요.")
+    e = Embed(title="POX 2021", url="https://ctf.powerofxx.org", description="안녕하세요! POX 2021 도우미 입니다.", color=0x1d87d7)
+    e.add_field(name="플래그 주세요 #1", value=flag_1, inline=False)
+    e.set_footer(text="CopyRight Demon TEAM & SISS")
+    await ctx.author.send(embed=e)
 
 @app.command()
 async def flag2(ctx):
     try:
-        if channel_check(ctx.channel.id) != False:
-            organizer = get(ctx.guild.roles, name="Organizer")
-            if organizer in ctx.author.roles:
-                e = Embed(title="POX 2021", url="https://ctf.powerofxx.org", description="안녕하세요! POX 2021 도우미 입니다.", color=0x1d87d7)
-                e.add_field(name="플래그 주세요 #2", value=flag_2, inline=False)
-                e.set_footer(text="CopyRight Demon TEAM & SISS")
-            else:
-                e = Embed(title="POX 2021", url="https://ctf.powerofxx.org", description="안녕하세요! POX 2021 도우미 입니다.", color=0xff0000)
-                e.add_field(name="오류!", value="인증되지 않은 사용자 입니다.", inline=False)
-                e.set_footer(text="CopyRight Demon TEAM & SISS")
-                await ctx.author.send(embed=e)
+        organizer = get(ctx.guild.roles, name="Organizer")
+        if organizer in ctx.author.roles:
+            e = Embed(title="POX 2021", url="https://ctf.powerofxx.org", description="안녕하세요! POX 2021 도우미 입니다.", color=0x1d87d7)
+            e.add_field(name="플래그 주세요 #2", value=flag_2, inline=False)
+            e.set_footer(text="CopyRight Demon TEAM & SISS")
         else:
-            await ctx.send("명령어를 사용할 수 없는 채널입니다. #flag봇 채널을 사용해주세요.")
+            e = Embed(title="POX 2021", url="https://ctf.powerofxx.org", description="안녕하세요! POX 2021 도우미 입니다.", color=0xff0000)
+            e.add_field(name="오류!", value="인증되지 않은 사용자 입니다.", inline=False)
+            e.set_footer(text="CopyRight Demon TEAM & SISS")
+            await ctx.author.send(embed=e)
     
     except AttributeError:
         e = Embed(title="POX 2021", url="https://ctf.powerofxx.org", description="안녕하세요! POX 2021 도우미 입니다.", color=0xff0000)
@@ -79,20 +67,17 @@ async def flag2(ctx):
 
 @app.command()
 async def verify(ctx):
-    if channel_check(ctx.channel.id) != False:
-        organizer = get(ctx.guild.roles, name="Organizer")
-        if organizer in ctx.author.roles:
-            e = Embed(title="POX 2021", url="https://ctf.powerofxx.org", description="안녕하세요! POX 2021 도우미 입니다.", color=0x1d87d7)
-            e.add_field(name="Check Permission", value=str(ctx.author) + " 님은 관리자입니다.", inline=False)
-            e.set_footer(text="CopyRight Demon TEAM & SISS")
-            await ctx.send(embed=e)
-        else:
-            e = Embed(title="POX 2021", url="https://ctf.powerofxx.org", description="안녕하세요! POX 2021 도우미 입니다.", color=0xff0000)
-            e.add_field(name="오류!", value=str(ctx.author) + " 님은 인증되지 않은 사용자 입니다. (your not Organizer permission)", inline=False)
-            e.set_footer(text="CopyRight Demon TEAM & SISS")
-            await ctx.send(embed=e)
+    organizer = get(ctx.guild.roles, name="Organizer")
+    if organizer in ctx.author.roles:
+        e = Embed(title="POX 2021", url="https://ctf.powerofxx.org", description="안녕하세요! POX 2021 도우미 입니다.", color=0x1d87d7)
+        e.add_field(name="Check Permission", value=str(ctx.author) + " 님은 관리자입니다.", inline=False)
+        e.set_footer(text="CopyRight Demon TEAM & SISS")
+        await ctx.send(embed=e)
     else:
-        await ctx.send("명령어를 사용할 수 없는 채널입니다. #flag봇 채널을 사용해주세요.")
+        e = Embed(title="POX 2021", url="https://ctf.powerofxx.org", description="안녕하세요! POX 2021 도우미 입니다.", color=0xff0000)
+        e.add_field(name="오류!", value=str(ctx.author) + " 님은 인증되지 않은 사용자 입니다. (your not Organizer permission)", inline=False)
+        e.set_footer(text="CopyRight Demon TEAM & SISS")
+        await ctx.send(embed=e)
 
 if __name__ == "__main__":
     app.run(token_load())
